3eb2f28d392725a11458f93f3f004bbeb07054ce
tranthidiep142 <tranthidiep142@gmail.com>
2022-12-08 21:46:04 +0700

    fix btn router bug ui

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -7,17 +7,19 @@ const styles = {
     productList: 'md:my-4 my-2',
     productListHeading: 'font-semibold text-left text-lg mx-6 border-b-2 border-double border-yellow-500',
     productListContainer: 'flex flex-wrap',
-    productItem: 'basis-1/2 md:basis-1/5 p-6 text-left',
+    // productItem: 'basis-1/2 md:basis-1/5 p-6 pb-16 text-left',
+    productItem: 'relative basis-1/2 md:basis-1/5 p-6 pb-16 text-left',
     productImg: 'm-auto',
     productName: 'text-sm lg:text-base',
     // productName: "text-sm lg:text-base line-clamp-3",
-    productPriceContainer: 'flex justify-between text-sm text-red-600',
+    // productPriceContainer: 'flex justify-between text-sm text-red-600',
+    productPriceContainer: 'flex justify-between gap-x-1 md:gap-x-8 text-sm text-red-600 absolute bottom-10',
     productPrice: 'font-semibold',
     productDiscount: 'font-bold',
+    productAction: 'absolute bottom-0',
     productCart: 'flex text-white pt-4',
     productCartContent:
-        'text-xs bg-indigo-500 px-3 py-2 rounded-md hover:cursor-pointer animate-bounce hover:animate-none hover:text-white hover:bg-yellow-500 transition ease-in-out delay-100 duration-200',
-    // productCartContent: "text-xs px-3 py-2 rounded-md hover:cursor-pointer transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 hover:text-white",
+        'text-xs px-3 py-2 rounded-md hover:cursor-pointer transition ease-in-out delay-100 bg-indigo-500 hover:-translate-y-1 hover:scale-110 duration-200 hover:text-white',
     productLikeIcon: 'flex',
     // productCartIcon: "w-5 text-red block"
 };
@@ -37,9 +39,7 @@ function ProductList(props) {
                         <div className={styles.productDiscount}>-24%</div>
                     </div>
                     <div className={styles.productAction}>
-                        {/* <div className={styles.productLikeIcon}><HeartOutlined /></div> */}
                         <div className={styles.productCart}>
-                            {/* <div className={styles.productCartIcon}><ShoppingCartOutlined /></div> */}
                             <div className={styles.productCartContent}>Add to cart</div>
                         </div>
                     </div>
